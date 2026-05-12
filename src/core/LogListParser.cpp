b7c71dd6ad73afb240cf1a93b8d2247384e17e91
Datura <daturasoldev@gmail.com>
2026-05-12 15:00:12 +0000

    feat passed on tests, i still need to test reading from an actual file, i will add one in mocks

--- src/core/LogListParser.cpp
+++ src/core/LogListParser.cpp
@@ -1,1 +1,37 @@
 // src/core/LogListParser.cpp
+
+#include <fstream>
+#include <istream>
+#include <monitor/LogListParser.hpp>
+#include <string>
+
+namespace monitor {
+std::vector<std::string> LogListParser::getLogPaths(std::istream& inStream) {
+  std::vector<std::string> paths;
+  std::string line;
+
+  // This will quit once finds the end of the file.
+  while (std::getline(inStream, line)) {
+    // skip empty lines
+    if (line.empty()) {
+      continue;
+    }
+    paths.push_back(line);
+  }
+
+  return paths;
+}
+
+std::vector<std::string> LogListParser::getLogPaths(
+    const std::string& masterFilePath) {
+  std::ifstream file(masterFilePath);
+
+  // empty vector if no file
+  if (!file.is_open()) {
+    return {};
+  }
+
+  return getLogPaths(file);
+}
+
+}  // namespace monitor
